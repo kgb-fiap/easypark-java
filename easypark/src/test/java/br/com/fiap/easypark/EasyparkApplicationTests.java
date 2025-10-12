0cce43839aba23c2ef6beb2edde2a4aa83f42af1
gabrielCZz <gabriel.cruz2512@gmail.com>
2025-10-12 17:09:45 -0300

    primeiro commit easypark

---
+++ easypark/src/test/java/br/com/fiap/easypark/EasyparkApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.easypark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
