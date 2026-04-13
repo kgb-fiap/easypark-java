230b4dc3ebef495e4db3f7c7c17880e42415b994
gabrielCZz <gabriel.cruz2512@gmail.com>
2026-04-12 23:48:29 -0300

    add Redirect

---
+++ easypark/src/main/java/br/com/fiap/easypark/controller/web/RootRedirectController.java
@@ -0,0 +1,13 @@
+package br.com.fiap.easypark.controller.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RootRedirectController {
+
+    @GetMapping("/")
+    public String index() {
+        return "redirect:/web";
+    }
+}
